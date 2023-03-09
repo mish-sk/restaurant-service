ce7025f856edcc7e5bd459c97f8643ff61950405
Mykhailo Sotnikov <sotnikov.mykhail@gmail.com>
2023-03-09 17:54:53 +0100

    Add models to admin.py

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from restaurant.models import Cook, Dish, DishType
+
+
+@admin.register(Cook)
+class CookAdmin(admin.ModelAdmin):
+    list_display = UserAdmin.list_display
+
+
+admin.register(Dish)
+admin.register(DishType)
